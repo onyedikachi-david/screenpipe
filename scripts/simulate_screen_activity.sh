2980c2544f2c5ecedff38d8a48bbf96d4fa1cf1a
David Anyatonwu <davidanyatonwu@gmail.com>
2024-08-26 15:55:01 +0100

    screen capture issues fix
    Signed-off-by: David Anyatonwu <davidanyatonwu@gmail.com>

--- scripts/simulate_screen_activity.sh
+++ scripts/simulate_screen_activity.sh
@@ -2,16 +2,25 @@
 
 # Check if running on Ubuntu
 if [[ "$(uname)" == "Linux" ]]; then
-  # Create a simple window
-  xterm -e "echo 'Screenpipe Test Window'; sleep 60" &
+  # Create a simple window using xterm
+  xterm -e "echo 'Screenpipe Test Window'; sleep 300" &
+  
+  # Wait for xterm to start
+  sleep 2
+
+  # Get the window ID of the xterm window
+  WINDOW_ID=$(xdotool search --name "Screenpipe Test Window" | head -n 1)
+
+  # Activate the window
+  xdotool windowactivate $WINDOW_ID
 
   # Simulate mouse movement
   for i in {1..10}; do
-    xdotool mousemove $((RANDOM % 1920)) $((RANDOM % 1080))
+    xdotool mousemove --window $WINDOW_ID $((RANDOM % 500)) $((RANDOM % 300))
     sleep 1
   done
 
   # Simulate typing
-  xdotool type "This is a test of screenpipe CLI"
-  xdotool key Return
+  xdotool type --window $WINDOW_ID "This is a test of screenpipe CLI"
+  xdotool key --window $WINDOW_ID Return
 fi
