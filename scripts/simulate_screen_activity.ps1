7facab8ded33530fc3ab9d85223c883ac57fe275
David Anyatonwu <davidanyatonwu@gmail.com>
2024-09-06 04:23:59 +0100

    changes
    Signed-off-by: David Anyatonwu <davidanyatonwu@gmail.com>

--- scripts/simulate_screen_activity.ps1
+++ scripts/simulate_screen_activity.ps1
@@ -3,8 +3,8 @@ Add-Type -AssemblyName System.Windows.Forms
 while ($true) {
     # Simulate mouse movement
     for ($i = 0; $i -lt 20; $i++) {
-        $x = Get-Random -Minimum 0 -Maximum 1024
-        $y = Get-Random -Minimum 0 -Maximum 768
+        $x = Get-Random -Minimum 0 -Maximum ([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width)
+        $y = Get-Random -Minimum 0 -Maximum ([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
         [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
         Start-Sleep -Milliseconds 200
     }
